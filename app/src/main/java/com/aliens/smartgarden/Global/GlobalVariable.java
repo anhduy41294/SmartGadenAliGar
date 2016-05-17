54d79417066c9a58914c4500c7538e6fb80766b8
Đinh Lê Mạnh Duy <manhduydl@gmail.com>
2016-05-17 16:11:25 +0700

    update Chart

---
+++ app/src/main/java/com/aliens/smartgarden/Global/GlobalVariable.java
@@ -0,0 +1,9 @@
+package com.aliens.smartgarden.Global;
+
+/**
+ * Created by Duy on 17-May-16.
+ */
+public class GlobalVariable {
+    public Boolean isTuoiNuoc = false;
+    public Boolean isManChe = false;
+}

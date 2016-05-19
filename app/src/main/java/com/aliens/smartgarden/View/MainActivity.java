5a333b8208d6165459817f77ad7c121146848948
Đinh Lê Mạnh Duy <manhduydl@gmail.com>
2016-05-19 16:18:56 +0700

    update ui

--- app/src/main/java/com/aliens/smartgarden/View/MainActivity.java
+++ app/src/main/java/com/aliens/smartgarden/View/MainActivity.java
@@ -62,15 +62,15 @@ public class MainActivity extends AppCompatActivity
         globalVariable = new GlobalVariable();
         setUpSpinner();
         setUpButton();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        assert fab != null;
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         nhietDoTxt = (TextView) findViewById(R.id.txtTemperature);
         doAmTxt = (TextView) findViewById(R.id.txtHumidity);
 

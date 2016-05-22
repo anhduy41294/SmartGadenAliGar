97d9844c6e78cb3eb5d09b5c484707c5904d1601
Đinh Lê Mạnh Duy <manhduydl@gmail.com>
2016-05-22 14:06:13 +0700

    update profile activity

--- app/src/main/java/com/aliens/smartgarden/View/AllProfileView/AllProfileActivity.java
+++ app/src/main/java/com/aliens/smartgarden/View/AllProfileView/AllProfileActivity.java
@@ -1,22 +1,13 @@
 package com.aliens.smartgarden.View.AllProfileView;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-import com.aliens.smartgarden.Controller.LoaderHelper;
 import com.aliens.smartgarden.Global.GlobalVariable;
-import com.aliens.smartgarden.Model.Profile;
-import com.aliens.smartgarden.Model.RecordSituation;
 import com.aliens.smartgarden.R;
-
-import java.util.ArrayList;
 
 public class AllProfileActivity extends AppCompatActivity {
 
@@ -34,36 +25,36 @@ public class AllProfileActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rv.setLayoutManager(linearLayoutManager);
 
-        AllProfileAdapter allProfileAdapter = new AllProfileAdapter(GlobalVariable.allProfile);
+        AllProfileAdapter allProfileAdapter = new AllProfileAdapter(GlobalVariable.allProfile, this);
         rv.setAdapter(allProfileAdapter);
 
     }
 
-    public class getAllProfile extends AsyncTask<String, Void, ArrayList<Profile>> {
-
-        public getAllProfile() {
-
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected ArrayList<Profile> doInBackground(String... params) {
-
-            LoaderHelper loaderHelper = new LoaderHelper();
-            return loaderHelper.getAllProfile();
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Profile> allProfile) {
-            super.onPostExecute(allProfile);
-
-            //Set value to UI
-            AllProfileAdapter allProfileAdapter = new AllProfileAdapter(allProfile);
-            rv.setAdapter(allProfileAdapter);
-        }
-    }
+//    public class getAllProfile extends AsyncTask<String, Void, ArrayList<Profile>> {
+//
+//        public getAllProfile() {
+//
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected ArrayList<Profile> doInBackground(String... params) {
+//
+//            LoaderHelper loaderHelper = new LoaderHelper();
+//            return loaderHelper.getAllProfile();
+//        }
+//
+//        @Override
+//        protected void onPostExecute(ArrayList<Profile> allProfile) {
+//            super.onPostExecute(allProfile);
+//
+//            //Set value to UI
+//            AllProfileAdapter allProfileAdapter = new AllProfileAdapter(allProfile);
+//            rv.setAdapter(allProfileAdapter);
+//        }
+//    }
 }
